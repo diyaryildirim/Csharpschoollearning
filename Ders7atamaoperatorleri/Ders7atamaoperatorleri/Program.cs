f5401a833dbe6ecafc8a96e5c358894d418b4ede
diyaryildirim <diyary2000@gmail.com>
2020-12-26 19:47:39 +0300

    New Lesson7
    Assignment  Operator

--- Ders7atamaoperatorleri/Ders7atamaoperatorleri/Program.cs
+++ Ders7atamaoperatorleri/Ders7atamaoperatorleri/Program.cs
@@ -10,6 +10,36 @@ namespace Ders7atamaoperatorleri
     {
         static void Main(string[] args)
         {
+            int sayi = 5;
+
+            Console.WriteLine(sayi);
+            sayi = sayi + 3;
+            Console.WriteLine(sayi);
+            sayi += 3;
+            Console.WriteLine(sayi);
+            sayi = sayi - 3;
+            Console.WriteLine(sayi);
+            sayi -= 3;
+            Console.WriteLine(sayi);
+
+            double sayi2 = 10;
+
+            Console.WriteLine(sayi2);
+            sayi2 = sayi2 / 2;
+            Console.WriteLine(sayi2);
+            sayi2 /= 2;
+            Console.WriteLine(sayi2);
+            sayi2 = sayi2 * 2;
+            Console.WriteLine(sayi2);
+            sayi2 *= 2;
+            Console.WriteLine(sayi2);
+            sayi2 = sayi2 % 2;
+            Console.WriteLine(sayi2);
+
+
+
+            Console.ReadLine();
+
         }
     }
 }
