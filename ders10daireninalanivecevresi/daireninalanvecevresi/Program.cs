ce5a5491224724c5ca59a13b2728a32406f6c93f
Diyar Yıldırım <Diyary2000@gmail.com>
2021-01-16 11:42:06 +0300

    new lesson

---
+++ ders10daireninalanivecevresi/daireninalanvecevresi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace daireninalanvecevresi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.14;
+            double daireninYaricapi;
+
+            Console.WriteLine("Lütfen Dairenin yarıçapını giriniz.");
+            daireninYaricapi = Convert.ToDouble(Console.ReadLine());
+
+
+            double daireninCevresi = 2 * PI * daireninYaricapi;
+            double daireninAlani = PI * daireninYaricapi * daireninYaricapi;
+
+
+            Console.WriteLine("Dairenin Çevresi = " + daireninCevresi);
+            Console.WriteLine("Dairenin Alanı = " + daireninAlani);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
