67f43de2bc55a57568de8e54f3b4a23a6ee38042
Diyar Yıldırım <Diyary2000@gmail.com>
2021-01-16 11:39:11 +0300

    new info

---
+++ ders12mathsinifi/Der11mathsinifi/Der11mathsinifi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Der11mathsinifi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sayi = 18;
+
+            Console.WriteLine(Math.Pow(sayi,3));
+            Console.ReadLine();
+        }
+    }
+}
