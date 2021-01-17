92d4e1175bab35074c2fbd327b28dee2069a124b
Diyar Yıldırım <diyary2000@gmail.com>
2021-01-17 15:34:00 +0300

    new lesson

---
+++ ders14dash1oyun/ders14dash1oyun/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ders14dash1oyun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rastgele = new Random();
+            int sayi1 = rastgele.Next(1, 2);
+            int sayi2 = rastgele.Next(1, 2);
+
+
+        }
+    }
+}
