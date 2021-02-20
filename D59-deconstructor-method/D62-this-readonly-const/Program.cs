605b8b7221c80fbda605aba3b2bdaf07fb45c8ae
Diyar Yıldırım <Diyary2000@gmail.com>
2021-02-20 17:50:12 +0300

    New Lesoon

--- D59-deconstructor-method/D62-this-readonly-const/Program.cs
+++ D59-deconstructor-method/D62-this-readonly-const/Program.cs
@@ -4,9 +4,30 @@ namespace D62_this_readonly_const
 {
     class Program
     {
+        //this readonly
+        class Isci
+        {
+            public readonly double maas;
+            public Isci(double maas)
+            {
+                this.maas = maas;
+            }
+        }
+
+        //1. Readonly constructor içerisinden değişebilir.
+        //2. Readonly sabit değeri sonradan değer atanabilir
+                //const sabitinin değeri oluşturulduğu anda verilmek zorundadır.
+        //3. Readonly matematiksel işlemlerin sonucunda değerine kavuşabilir.
+                //const matematiksel işlemlerin sonucunda değerine kavuşmaz.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Isci i1 = new Isci(5500.50);
+            Isci i2 = new Isci(2200.6);
+
+            Console.WriteLine(i1.maas + " " + i2.maas);
+
+           Console.ReadLine();
         }
     }
 }
